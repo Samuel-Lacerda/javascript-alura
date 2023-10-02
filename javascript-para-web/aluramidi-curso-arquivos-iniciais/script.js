f68eb4783a819ee2d3dee9b1649aeeb07928a010
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2023-10-01 22:03:30 -0300

    Update script.js

--- javascript-para-web/aluramidi-curso-arquivos-iniciais/script.js
+++ javascript-para-web/aluramidi-curso-arquivos-iniciais/script.js
@@ -13,10 +13,12 @@ for (let contador = 0; contador < ListaDeTeclas.length; contador++){
     tecla.onclick = function (){
         tocaSom(idAudio);
     }
-    console.log(idAudio);
+    // console.log(idAudio);
 
-    tecla.onkeydown = function (){
-        tecla.classList.add('ativa');
+    tecla.onkeydown = function (evento){
+        if(evento.code === 'Enter' || evento.code === 'Space'){
+            tecla.classList.add('ativa');
+        }
     }
     tecla.onkeyup = function (){
         tecla.classList.remove('ativa');
