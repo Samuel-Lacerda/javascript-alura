606f2f5e55d21eb083d4ce2546bc01659eb8205b
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2023-10-02 22:19:28 -0300

    Update script.js

--- javascript-para-web/aluramidi-curso-arquivos-iniciais/script.js
+++ javascript-para-web/aluramidi-curso-arquivos-iniciais/script.js
@@ -1,5 +1,15 @@
-function tocaSom(idElementAudio){
-    document.querySelector(idElementAudio).play();
+function tocaSom(seletorAudio){
+   const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio'){     
+        elemento.play();
+    }
+    else{
+        console.log('Elemento não encontrado ou seletor inválido.');
+    }
+    
+    
+
 }
 
 const ListaDeTeclas = document.querySelectorAll('.tecla');
