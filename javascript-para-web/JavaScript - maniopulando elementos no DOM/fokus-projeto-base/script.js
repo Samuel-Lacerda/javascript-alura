a6bbaf5e87e02fd26ceca9365feb303ee4951a1c
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2023-11-26 21:06:54 -0300

    update

---
+++ javascript-para-web/JavaScript - maniopulando elementos no DOM/fokus-projeto-base/script.js
@@ -0,0 +1,4 @@
+const html = document.querySelector('html');
+const focoBtn = document.querySelector('.app__card-button--foco');
+const curtoBtn = document.querySelector('.app__card-button--curto');
+const longoBtn = document.querySelector('.app__card-button--longo');
