0798407448405473dca83ba10406680aafbeecf3
Samuel Lacerda <127802525+Samuel-Lacerda@users.noreply.github.com>
2023-11-27 11:48:36 -0300

    Update script.js

--- javascript-para-web/JavaScript - maniopulando elementos no DOM/fokus-projeto-base/script.js
+++ javascript-para-web/JavaScript - maniopulando elementos no DOM/fokus-projeto-base/script.js
@@ -2,3 +2,30 @@ const html = document.querySelector('html');
 const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
+const img = document.querySelector('.app__image');
+
+
+
+focoBtn.addEventListener('click',() =>{
+    focoBtn.classList.add('active');
+    curtoBtn.classList.remove('active');
+    longoBtn.classList.remove('active');
+    html.setAttribute('data-contexto','foco');
+    img.setAttribute('src','imagens/foco.png');
+})
+
+curtoBtn.addEventListener('click', () =>{
+    focoBtn.classList.remove('active');
+    curtoBtn.classList.add('active');
+    longoBtn.classList.remove('active');
+    html.setAttribute('data-contexto','descanso-curto');
+    img.setAttribute('src','imagens/descanso-curto.png');
+})
+
+longoBtn.addEventListener('click',()=>{
+    focoBtn.classList.remove('active');
+    curtoBtn.classList.remove('active');
+    longoBtn.classList.add('active');
+    html.setAttribute('data-contexto','descanso-longo');
+    img.setAttribute('src','imagens/descanso-longo.png');
+})
